1ddf6467ce56f6d96c70b7204fe16de104b8ebda
George Wu <uiharu@buaa.edu.cn>
2016-04-21 12:48:23 +0800

    Fixed 'unable to download' error, and add several other features.

--- Makefile
+++ Makefile
@@ -1,33 +1,40 @@
 # http://www.sqlite.org/download.html
 SQLITE_YEAR     ?= 2016
-SQLITE_VERSION  ?= 3120100
+SQLITE_VERSION  ?= 3120200
 SQLITE_BASENAME := sqlite-amalgamation-$(SQLITE_VERSION)
 SQLITE_URL      := http://www.sqlite.org/$(SQLITE_YEAR)/$(SQLITE_BASENAME).zip
+
+# This flag should be called in a local context, see jni/Android.mk.
+#CFLAGS += -D SQLITE_ENABLE_COLUMN_METADATA
 
 # NDK parameters.
 NDK_BUILD_EXEC := ndk-build
 # http://www.cnblogs.com/rainduck/p/4177384.html
 APP_PLATFORM := android-19
-APP_ABI := all
+# Use 'all' to build targets for all platforms supported by Android.
+APP_ABI := x86,armeabi-v7a
 
 # Build/Compile
 libs: build/sqlite3.c
-	$(NDK_BUILD_EXEC) APP_PLATFORM=$(APP_PLATFORM) APP_ABI=$(APP_ABI)
+    $(NDK_BUILD_EXEC) APP_PLATFORM=$(APP_PLATFORM) APP_ABI=$(APP_ABI)
 
 # Unpack
 build/sqlite3.c: $(SQLITE_BASENAME).zip
-	unzip -oq "$<"
-	rm -rf build
-	mv "$(SQLITE_BASENAME)" build
-	touch "$@"
+    unzip -oq "$<"
+    rm -rf build
+    mv "$(SQLITE_BASENAME)" build
+    touch "$@"
 
 # Download
 $(SQLITE_BASENAME).zip:
-	wget -N -c "$(SQLITE_URL)"
+    wget -N -c "$(SQLITE_URL)"
+
+echo:
+    echo $(SQLITE_URL)
 
 clean:
-	rm -f "$(SQLITE_BASENAME).zip"
-	rm -rf "$(SQLITE_BASENAME)"
-	rm -rf build
-	rm -rf obj
-	rm -rf libs
+    rm -f "$(SQLITE_BASENAME).zip"
+    rm -rf "$(SQLITE_BASENAME)"
+    rm -rf build
+    rm -rf obj
+    rm -rf libs
--- jni/Android.mk
+++ jni/Android.mk
@@ -1,22 +1,27 @@
-#APP_ABI := all
-#APP_PLATFORM := android-19
-
 LOCAL_PATH := $(call my-dir)
 
+#BUILD_STATIC := 1
+#BULID_STATIC_EXEC := 1
+BUILD_DYNAMIC := 1
+#BUILD_DYNAMIC_EXEC := 1
+
+SQLITE_ENABLE_COLUMN_METADATA ?= -DSQLITE_ENABLE_COLUMN_METADATA
 
 #
 # Statically Linked
 #
 
+ifdef BUILD_STATIC
 include $(CLEAR_VARS)
 LOCAL_MODULE            := sqlite3-a
 LOCAL_MODULE_FILENAME   := libsqlite3
 LOCAL_SRC_FILES         := ../build/sqlite3.c
 LOCAL_C_INCLUDES        := ../build
 LOCAL_EXPORT_C_INCLUDES := ../build
-LOCAL_CFLAGS            := -DSQLITE_THREADSAFE=1
+LOCAL_CFLAGS            := -DSQLITE_THREADSAFE=1 $(SQLITE_ENABLE_COLUMN_METADATA)
 include $(BUILD_STATIC_LIBRARY)
 
+ifdef BUILD_STATIC_EXEC
 include $(CLEAR_VARS)
 LOCAL_MODULE            := sqlite3-static-cli
 LOCAL_MODULE_FILENAME   := sqlite3-static
@@ -27,21 +32,24 @@ LOCAL_EXPORT_C_INCLUDES := ../build
 LOCAL_CFLAGS            := -DSQLITE_THREADSAFE=1 -fPIE
 LOCAL_LDFLAGS           := -fPIE -pie
 include $(BUILD_EXECUTABLE)
-
+endif
+endif
 
 #
 # Dynamically Linked
 #
 
+ifdef BUILD_DYNAMIC
 include $(CLEAR_VARS)
 LOCAL_MODULE            := sqlite3-so
 LOCAL_MODULE_FILENAME   := libsqlite3
 LOCAL_SRC_FILES         := ../build/sqlite3.c
 LOCAL_C_INCLUDES        := ../build
 LOCAL_EXPORT_C_INCLUDES := ../build
-LOCAL_CFLAGS            := -DSQLITE_THREADSAFE=1
+LOCAL_CFLAGS            := -DSQLITE_THREADSAFE=1 $(SQLITE_ENABLE_COLUMN_METADATA)
 include $(BUILD_SHARED_LIBRARY)
 
+ifdef BUILD_DYNAMIC_EXEC
 include $(CLEAR_VARS)
 LOCAL_MODULE            := sqlite3-dynamic-cli
 LOCAL_MODULE_FILENAME   := sqlite3-dynamic
@@ -52,3 +60,5 @@ LOCAL_EXPORT_C_INCLUDES := ../build
 LOCAL_CFLAGS            := -DSQLITE_THREADSAFE=1 -fPIE
 LOCAL_LDFLAGS           := -fPIE -pie
 include $(BUILD_EXECUTABLE)
+endif
+endif
